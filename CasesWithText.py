5d68fc68ebd9d62afc6f2a33558646d0a306bf41
AparnaAnand <aparnaan@usc.edu>
2017-02-20 21:03:16 -0800

    Delete

--- CasesWithText.py
+++
@@ -1,9 +0,0 @@
-class Cases:
-    def __init__(self,id,court,date,text):
-        self.case_id=id
-        self.court=court
-        self.case_date=date
-        self.text=text
-    def makeJSON(self):
-        return {"case_id":self.case_id,"court":self.court,"text":self.text}
-    #"statute":self.statute"case_date":self.case_date,
