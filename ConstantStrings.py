8eb92fb37b80ea29f1e3a23aa5e172e95125fa95
AparnaAnand <aparnaan@usc.edu>
2017-02-20 21:03:43 -0800

    Update

--- ConstantStrings.py
+++ ConstantStrings.py
@@ -1,13 +1,16 @@
+import os
+
 hostname = "localhost"
 username = "root"
 password = "yppa39213"
 databasename = "court_listener"
-postfix_json = "OP.json"
-postfix_aggr_fif = "AT_fifty.csv"
-postfix_tf_fif = "TF_fifty.csv"
-postfix_tf_dec = "TF_decade.csv"
-path_at_fifty = "AT_FILES/FIFTY"
-path_models_fifty = "MODEL_FILES/FIFTY"
-path_models_decade = "MODEL_FILES/DECADE"
-path_tf_fifty = "TF_FILES/FIFTY"
-path_tf_decade = "TF_FILES/DECADE"
+postfix_aggr_five = "_AT_five.csv"
+path_aggr_five = os.path.join("AT_FILES","FIVE")
+postfix_tf_five = "_TF_five.csv"
+postfix_tf_decade = "_TF_decade.csv"
+path_tf_five = os.path.join("TF_FILES","FIVE")
+path_tf_decade = os.path.join("TF_FILES","DECADE")
+postfix_models_five = "_MODEL_five"
+postfix_models_decade = "_MODEL_decade"
+path_models_five = os.path.join("MODEL_FILES","FIVE")
+path_models_decade = os.path.join("MODEL_FILES","DECADE")
